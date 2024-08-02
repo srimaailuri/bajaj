22c88ed279b7ca048a65622eeea73b034c0306d8
srima <as9731@srmist.edu.in>
2024-08-02 21:42:27 +0530

    added backend

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3002;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -42,7 +41,4 @@ app.get('/bfhl', (req, res) => {
     });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
-
+module.exports = app;
